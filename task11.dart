aa41b1b4d95be81b7c4546f29d1dfe5c9e515eb2
Zarlykov Baiel <88328136+zarlykovbaiel@users.noreply.github.com>
2022-12-26 08:23:47 +0600

    Add files via upload

---
+++ task11.dart
@@ -0,0 +1,41 @@
+import 'package:task11/task11.dart' as task11;
+
+// void main() {
+//   var v1 = Vehicle(maxSpeed: 181, mileAge: 280000, seatingCapacity: 23);
+//   var sc2 = v1;
+//   var bu2 = v1;
+//   print(bu2.color);
+// }
+
+// class Vehicle {
+//   int maxSpeed;
+//   int mileAge;
+//   int seatingCapacity;
+//   Vehicle(
+//       {required this.maxSpeed,
+//       required this.mileAge,
+//       required this.seatingCapacity});
+// }
+
+// class Color {
+//   void color() {
+//     print('white');
+//   }
+// }
+
+// class Scooter extends Vehicle with Color {
+//   Scooter({
+//     required super.maxSpeed,
+//     required super.mileAge,
+//     required super.seatingCapacity,
+//   });
+// }
+
+// class Bus extends Vehicle with Color {
+//   int seatingCapacity2 = 50;
+//   Bus(
+//       {required super.seatingCapacity,
+//       required super.maxSpeed,
+//       required super.mileAge,
+//       required this.seatingCapacity2});
+// }
